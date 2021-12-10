d15f2eff4ea08635e8dba10a96976b61747115bc
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2021-12-11 05:21:49 +0900

    Fix select form styling

--- components/select-form/select-form.tsx
+++ components/select-form/select-form.tsx
@@ -13,7 +13,7 @@ export const SelectForm: React.FC<Props> = ({label, options, ...props}) => {
       <select
         id="location"
         name="location"
-        className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+        className="mt-1 block w-full pl-3 pr-10 py-2 text-base focus:outline-none sm:text-sm rounded-md bg-stone-800 text-stone-300"
         {...props}
       >
         <option value="">{label}</option>
